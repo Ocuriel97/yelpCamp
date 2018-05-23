cc63249a4d50ede64730f9fc778c3c5b020fe915
Omar Curiel <omarcuriel@Omars-MacBook-Pro.local>
2018-05-22 17:54:56 -0700

    added mongodb

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 var app = require('express')()
 var request = require('request')
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 
-var campgrounds = [
-  {name: 'flats', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/1991-1996_BMW_318i_%28E36%29_sedan_%282011-04-02%29_01.jpg/1200px-1991-1996_BMW_318i_%28E36%29_sedan_%282011-04-02%29_01.jpg'},
-  {name: 'hospital flats', image: 'https://cdn.shopify.com/s/files/1/1063/6350/products/e36_frontside_profile_1024x1024.jpg?v=1502860554'},
-  {name: 'Crazy Horse', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM5WAxA9_0BXRmrwTwrL-HVhJVCBDtEgol1Ckbd0ohzdx-Kwmh'}
-]
+mongoose.connect('mongodb://localhost/yelp_camp')
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+})
+
+var campground = mongoose.model('Campground', campgroundSchema)
+
+// campground.create(
+//   {
+//     name: 'flats',
+//     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/1991-1996_BMW_318i_%28E36%29_sedan_%282011-04-02%29_01.jpg/1200px-1991-1996_BMW_318i_%28E36%29_sedan_%282011-04-02%29_01.jpg',
+//     description: "this flat is dope af"
+//   }, function(err, campground){
+//     if(err){
+//       console.log(err)
+//     } else {
+//       console.log('new campground')
+//       console.log(campground)
+//     }
+//   }
+// )
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
@@ -16,19 +36,42 @@ app.get('/', function(req,res){
 })
 
 app.get('/campgrounds', function(req,res){
-  res.render('campgrounds', {campgrounds: campgrounds})
+  campground.find({}, function(err,allCampgrounds){
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('index', {campgrounds: allCampgrounds})
+    }
+  })
+  //res.render('campgrounds', {campgrounds: campgrounds})
 })
 
 app.post('/campgrounds', function(req,res){
   var name = req.body.name
   var image = req.body.image
-  var newCamp = {name: name, image: image}
-  campgrounds.push(newCamp)
-  res.redirect('/campgrounds')
+  var desc = req.body.description
+  var newCamp = {name: name, image: image, description: desc}
+  campground.create(newCamp, function(err, newlyCreated){
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect('/campgrounds')
+    }
+  })
 })
 
 app.get('/campgrounds/new', function(req,res){
   res.render('new')
+})
+
+app.get('/campgrounds/:id', function(req,res){
+  campground.findById(req.params.id, function(err, foundCamp){
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('show', {campground: foundCamp})
+    }
+  })
 })
 
 app.listen(3000, function(){
